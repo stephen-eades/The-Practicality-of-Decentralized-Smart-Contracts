5868a1e4c15a13df31fe3958aabb05fed47c9921
stephen-eades <stephen.a.eades@gmail.com>
2021-04-13 18:29:07 -0400

    Added most of Jupyter Notebooks data analysis

--- app/src/components/BaseComponent.js
+++ app/src/components/BaseComponent.js
@@ -11,47 +11,10 @@ import HomeComponent from "./body/HomeComponent";
 import ViewComponent from "./body/ViewComponent";
 import FooterComponent from "./footer/FooterComponent";
 import { BrowserRouter, Switch } from 'react-router-dom';
-import { accountIndex } from './../test/_test.js'
-import Web3 from "web3";
 
 
 export default ({ drizzle, drizzleState }) => {
   // destructure drizzle and drizzleState from props
-
-
-  window.addEventListener('load', async () => {
-    console.log('testttt');
-    // Wait for loading completion to avoid race conditions with web3 injection timing.
-      if (window.ethereum) {
-        console.log('test');
-        const web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          web3.eth.sendTransaction({from: drizzleState.accounts[accountIndex]}); // remove this before deploying to prod});
-          console.log(web3);
-          // Acccounts now exposed
-          return web3;
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-        const web3 = window.web3;
-        console.log('Injected web3 detected.');
-        return web3;
-      }
-      // Fallback to localhost; use dev console port by default...
-      else {
-        const provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
-        const web3 = new Web3(provider);
-        console.log('No web3 instance injected, using Local web3.');
-        return web3;
-      }
-    });
-
 
   return (
     <DrizzleContext.Provider drizzle={drizzle}>
