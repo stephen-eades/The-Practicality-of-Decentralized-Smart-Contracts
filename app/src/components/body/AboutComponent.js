fc8ae163b61fbf39291a5c769e1253658461bc51
stephen-eades <stephen.a.eades@gmail.com>
2021-04-14 12:53:46 -0400

    updated about page with data

--- app/src/components/body/AboutComponent.js
+++ app/src/components/body/AboutComponent.js
@@ -1,4 +1,13 @@
 import React from "react";
+import eventManagerAndCreatorDeploy from "./../../EventManagerAndCreatorDeploy.png";
+import eventContractsCompare from "./../../EventContractsCompare.png";
+import lotteryIncreaseTicketDeploy from "./../../LotteryIncreaseTicketDeploy.png";
+import lotteryIncreaseTicketGas from "./../../LotteryIncreaseTicketGas.png";
+import escrowDepositTransaction from "./../../EscrowDepositTransaction.png";
+import pollVoteTransaction from "./../../PollVoteTransaction.png";
+import ethGwei1 from "./../../ethGwei1.png";
+import ethGwei2 from "./../../ethGwei2.png";
+import ethGwei3 from "./../../ethGwei3.png";
 
 
 export default ({ drizzle, drizzleState }) => {
@@ -22,24 +31,33 @@ export default ({ drizzle, drizzleState }) => {
           <p>
             The code for this application is in this <a href="https://github.com/stephen-eades/The-Practicality-of-Decentralized-Smart-Contracts">Github repository.</a> 
             <br></br><br></br>
-            Below is detailed analysis of the speed and costs of transactions for interacting with and deploying smart
+            Below is data regarding the costs of transactions for interacting with and deploying smart
             contracts on Crêpe. The Jupyter Notebook files can be found in the above repo as well:
           </p>
           <br></br><br></br>
           <br></br><br></br>
-          ...img...
+          <h3>EventManager & EventCreator Deployment</h3>
+          <img src={eventManagerAndCreatorDeploy} alt="event-manager-creator-deploy" className="data-img-class"/>
           <br></br><br></br>
           <br></br><br></br>
           <br></br><br></br>
-          <br></br><br></br>
-          ...img...
-          <br></br><br></br>
-          <br></br><br></br>
+          <h3>Poll, Escrow, & Lottery Contract Deployment</h3>
+          <img src={eventContractsCompare} alt="event-contract-deploy" className="data-img-class"/>
           <br></br><br></br>
           <br></br><br></br>
-          ...img...
+          <br></br><br></br>
+          <h3>Event Contract Transactions</h3>
+          <img src={lotteryIncreaseTicketDeploy} alt="lottery-increase-deploy-cost" className="data-img-class"/>
+          <img src={lotteryIncreaseTicketGas} alt="lottery-increase-deploy-gas" className="data-img-class"/>
+          <img src={escrowDepositTransaction} alt="escrow-deposit-transaction" className="data-img-class"/>
+          <img src={pollVoteTransaction} alt="poll-vote-transaction" className="data-img-class"/>
           <br></br><br></br>
           <br></br><br></br>
+          <br></br><br></br>
+          <h3>Ether and Gwei Correlation</h3>
+          <img src={ethGwei1} alt="eth-gwei-1" className="data-img-class"/>
+          <img src={ethGwei2} alt="eth-gwei-2" className="data-img-class"/>
+          <img src={ethGwei3} alt="eth-gwei-3" className="data-img-class"/>
         </div>
 
       </div>
